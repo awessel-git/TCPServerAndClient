1045ca0387c1ebbced732c66b167e22b8ab51f60
awessel-git <antonw.dev@gmail.com>
2025-09-16 03:45:06 +0200

    Add listener for TCP on Server code. No functionality yet.

--- TCPServer/Program.cs
+++ TCPServer/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+
+const int Port = 5000;
+
+var listener = new TcpListener(IPAddress.Any, Port); // Socket, listen for any IP address on this machine, on this port
+listener.Start();
+Console.WriteLine($"Server is running on port {Port}... (:");
+
+while (true)
+{
+    TcpClient client = await listener.AcceptTcpClientAsync(); // Client it waits for
+    Console.WriteLine("Client connected!");
+
+}
